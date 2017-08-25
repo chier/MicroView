031f22fa1729eb0029722fae34de107f064c6765
zhangzlg <zhangzhanliang@micro-view.com>
2017-08-25 15:35:25 +0800

    增加代理功能模块

---
+++ HER_Service/src/her/http/ProxyServlet.java
@@ -0,0 +1,70 @@
+package her.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+
+/**
+ * Servlet implementation class ProxyServlet
+ */
+//@WebServlet("/proxyServlet")
+public class ProxyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProxyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = request.getParameter("url");
+		String callback = request.getParameter("callback");
+		
+
+		response.setContentType("text/html;charset=UTF-8");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "0");
+		response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("XDomainRequestAllowed","1");
+		   
+		GetMethod postMethod = new GetMethod(url); 
+		new HttpClient().executeMethod(postMethod);
+		String result = postMethod.getResponseBodyAsString();
+		
+		PrintWriter writer = response.getWriter();
+		if(callback != null && callback.length() > 0){
+			result = callback + "("+ result +")";
+		}
+		writer.write(result);
+		writer.flush();
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
